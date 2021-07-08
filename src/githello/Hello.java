8ec75bd0f8774753069ace9e8de5dd001f8e05e9
leemg <matthew.lee0619@gmail.com>
2021-07-08 14:21:54 +0900

    충돌 테스트!!!

--- src/githello/Hello.java
+++ src/githello/Hello.java
@@ -4,9 +4,13 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello");
+		
 		System.out.println("안녕하세요");
+		System.out.println("hello");
 		System.out.println("깃헙에 커밋하기 테스트");
+		System.out.println("월요일부터 비대면수업.");
+		
+		
 	}
 
 }
