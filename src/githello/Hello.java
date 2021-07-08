ec7bf6c3d9c472106d257ba745b54208db8551b4
leemg <matthew.lee0619@gmail.com>
2021-07-08 14:28:49 +0900

    충돌테스트

--- src/githello/Hello.java
+++ src/githello/Hello.java
@@ -4,20 +4,14 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
 		
 		System.out.println("¾È³çÇÏ¼¼¿ä");
 		System.out.println("hello");
 		System.out.println("±êÇé¿¡ Ä¿¹ÔÇÏ±â Å×½ºÆ®");
 		System.out.println("¿ù¿äÀÏºÎÅÍ ºñ´ë¸é¼ö¾÷.");
 		
-		
-=======
-		System.out.println("hello");
-		System.out.println("ì•ˆë…•í•˜ì„¸ìš”");
-		System.out.println("ê¹ƒí—™ì— ì»¤ë°‹í•˜ê¸° í…ŒìŠ¤íŠ¸");
-		System.out.println("ì¶©ëŒ í…ŒìŠ¤íŠ¸ ê½!");
->>>>>>> branch 'master' of https://github.com/leopardmilky/gitjava.git
+		System.out.println("Ãæµ¹ Å×½ºÆ® ²Î!");
+
 	}
 
 }
